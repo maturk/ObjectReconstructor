6f0730f56b0ebb8400d44a58e898cd46c928a5de
maturk <matiasturkulainen@gmail.com>
2022-11-08 20:12:01 +0100

    voxel decoder

--- models/models.py
+++ models/models.py
@@ -2,10 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from torchvision.models.resnet import resnet18
-from torchvision.models import feature_extraction
-from torchvision.models.feature_extraction import get_graph_node_names
-from torchvision.models.feature_extraction import create_feature_extractor
+
 
 class PointCloudEncoder(nn.Module):
     def __init__(self, emb_dim):
@@ -20,7 +17,6 @@ class PointCloudEncoder(nn.Module):
     def forward(self, xyzs): 
         """
         Args:
-            xyz: (B, 3, N)
             xyzs = (B, num_views, 3, N)
         """     
         batch_embeddings = torch.empty((xyzs.shape[0], xyzs.shape[1], self.emb_dim)).to(xyzs.device)
@@ -51,7 +47,6 @@ class PointCloudDecoder(nn.Module):
         """
         Args:
             embedding: (B, 512)
-
         """
         bs = embedding.size()[0]
         out = F.relu(self.fc1(embedding))
@@ -70,14 +65,12 @@ class PointCloudAE(nn.Module):
     def forward(self, in_pcs, emb=None):
         """
         Args:
-            in_pc: (B, N, 3)
             in_pcs: (B, num_views, N, 3)
             emb: (B, 512)
 
         Returns:
             emb: (B, emb_dim)
             out_pc: (B, n_pts, 3)
-
         """
         if emb is None:
             xyzs = in_pcs.permute(0, 1, 3, 2)   
@@ -86,3 +79,82 @@ class PointCloudAE(nn.Module):
         return emb, out_pc
 
 
+class VoxelDecoder(nn.Module):
+    def __init__(self, emb_dim = 512, voxel_size = 128):
+        super(VoxelDecoder, self).__init__()
+        self.voxel_size = voxel_size
+        self.fc1 = nn.Linear(emb_dim, 512)
+        self.fc2 = nn.Linear(512, 1024)
+        
+        # out_dimension = (Hin−1)×stride[0]−2×padding[0]+dilation[0]×(kernel_size[0]−1)+output_padding[0]+1
+        if self.voxel_size == 64:
+            self.deconv = nn.Sequential(
+                    nn.ConvTranspose2d(1, 32, kernel_size=3, stride = 1, padding=1, output_padding= 0), 
+                    nn.LeakyReLU(),
+                    nn.ConvTranspose2d(32, 64, kernel_size=3, stride = 2, padding=1, output_padding= 1), 
+                    nn.LeakyReLU(),
+                    )
+        if self.voxel_size == 128:
+            self.deconv = nn.Sequential(
+                    nn.ConvTranspose2d(1, 32, kernel_size=3, stride = 1, padding=1, output_padding= 0), 
+                    nn.LeakyReLU(),
+                    nn.ConvTranspose2d(32, 64, kernel_size=3, stride = 2, padding=1, output_padding= 1), 
+                    nn.LeakyReLU(),
+                    nn.ConvTranspose2d(64, 128, kernel_size=3, stride =2, padding=1, output_padding= 1),
+                    nn.LeakyReLU()
+                    )
+        if self.voxel_size == 256:
+            self.deconv = nn.Sequential(
+                    nn.ConvTranspose2d(1, 32, kernel_size=3, stride = 1, padding=1, output_padding= 0), 
+                    nn.LeakyReLU(),
+                    nn.ConvTranspose2d(32, 64, kernel_size=3, stride = 2, padding=1, output_padding= 1), 
+                    nn.LeakyReLU(),
+                    nn.ConvTranspose2d(64, 128, kernel_size=3, stride =2, padding=1, output_padding= 1),
+                    nn.LeakyReLU(),
+                    nn.ConvTranspose2d(128, 256, kernel_size=3, stride =2, padding=1, output_padding= 1),
+                    nn.LeakyReLU()
+            )
+            
+        self.predictor = nn.Conv2d(self.voxel_size, self.voxel_size, kernel_size=1, stride=1, padding=0)
+        
+    def forward(self, embedding):
+        """
+        Args:
+            embedding: (B, 512)
+            out_voxel: (B, voxel_size, voxel_size, voxel_size)
+        """
+        bs = embedding.size()[0]
+        out = F.relu(self.fc1(embedding))
+        out = F.relu(self.fc2(out))
+        out = out.view(bs, 1, 32, 32) # lifting from 1D to 2D
+        for layer in self.deconv:
+            
+            out = layer(out)
+        out_voxel = self.predictor(out)
+        return out_voxel
+    
+    
+class VoxelAE(nn.Module):
+    def __init__(self, emb_dim=512, voxel_size=128):
+        super(VoxelAE, self).__init__()
+        self.encoder = PointCloudEncoder(emb_dim)
+        self.decoder = VoxelDecoder(emb_dim, voxel_size)
+
+    def forward(self, in_pcs, emb=None):
+        """
+        Args:
+            in_pcs: (B, num_views, N, 3)
+            emb: (B, 512)
+
+        Returns:
+            emb: (B, emb_dim)
+            out_voxel: (B, voxel_size, voxel_size, voxel_size)
+        """
+        if emb is None:
+            xyzs = in_pcs.permute(0, 1, 3, 2)   
+            emb = self.encoder(xyzs)
+        out_voxel = self.decoder(emb)
+        return emb, out_voxel
+
+if __name__ == "__main__":
+    pass
