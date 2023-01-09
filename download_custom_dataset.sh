aa578b9eb8e02e3b136dfda3475b3ece8f35d26f
maturk <matiasturkulainen@gmail.com>
2023-01-09 01:00:24 +0100

    remove old file

--- download_custom_dataset.sh
+++
@@ -1,9 +0,0 @@
-# init folder
-mkdir -p data
-
-# download custom ShapeNet multi-view dataset for training
-cd data
-wget ftp://213.144.153.67/ObjectReconstructor/small_train_test_split.tar.gz
-tar zxvf small_train_test_split.tar.gz
-rm small_train_test_split.tar.gz
-cd ..
