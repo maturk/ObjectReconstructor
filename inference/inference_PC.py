96856d5db63bded8505852204e1034d79cd97e59
maturk <matiasturkulainen@gmail.com>
2023-01-27 01:32:10 +0100

    improved inference scripts

--- inference/inference_PC.py
+++ inference/inference_PC.py
@@ -5,54 +5,75 @@ Run inference with a pretrained model for point cloud reconstruction
 import torch
 import argparse
 import os
-from ObjectReconstructor.datasets import BlenderDataset
-from ObjectReconstructor.models.point_ae import PointCloudAE, PointFusionAE
 import numpy as np
-from pytorch3d.loss import chamfer_distance
-from torch.utils.data import random_split
 import open3d as o3d
+from pytorch3d.loss import chamfer_distance
 import csv
 import pandas
+from ObjectReconstructor.datasets import BlenderDataset
+from ObjectReconstructor.models.point_ae import PointCloudAE, PointFusionAE
+from ObjectReconstructor.scratch import vis
 
-#views_15_points_1024_emb_dim_256_epoch_1800, 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--visualize', type=bool, default=False, help='Set to true if you want to visualize registration for each test object')
-parser.add_argument('--tsne', type=bool, default=True, help='Set to true if you want to visualize T-SNE embeddings')
+parser.add_argument('--save_renders', type=bool, default=True, help='Render voxel grid reconstructions and save them to results folder')
+parser.add_argument('--tsne', type=bool, default=False, help='Set to true if you want to visualize T-SNE embeddings')
 parser.add_argument('--fusion', type=bool, default=False, help='Set to true if fusion model is to be used')
 parser.add_argument('--num_points', type=int, default=1024, help='number of points in point cloud')
 parser.add_argument('--emb_dim', type=int, default=256, help='dimension of latent embedding')
 parser.add_argument('--device', type=str, default='cuda:0', help='GPU to use')
-parser.add_argument('--load_model', type=str, default=f'{os.getcwd()}/results/point_clouds/models/views_15_points_1024_emb_dim_256_epoch_800.pth', help='load model path')
+parser.add_argument('--load_model', type=str, default=f'{os.getcwd()}/results/point_clouds/models/views_15_points_1024_emb_dim_256_epoch_1800.pth', help='load model path')
 parser.add_argument('--result_dir', type=str, default=f'{os.getcwd()}/inference/results/point_clouds', help='directory to save inference results')
 parser.add_argument('--save_dir', type=str, default=f'{os.getcwd()}/data/train_test_split/train', help='save directory of images to be used for inference (preprocessed shapenet images)')
-parser.add_argument('--num_views', type=int, default=1, help = 'Number of input views per object instance, default set to 1 for single shot inference')
+parser.add_argument('--num_views', type=int, default=5, help = 'Number of input views per object instance, default set to 1 for single shot inference')
+
+
+names = {'03211117':'display', '02992529':'cellphone', '02954340':'cap', '02942699':'camera', '02946921':'can', '03797390':'mug', '02880940':'bowl',
+         '3211117':'display', '2992529':'cellphone', '2954340':'cap', '2942699':'camera', '2946921':'can', '3797390':'mug', '2880940':'bowl'}
 
 
 def inference(model, test_object):
     with torch.no_grad():
-        test = test_object
-        depths = test['depths'].clone().detach().to(device=opt.device, dtype= torch.float)
+        data = test_object
+        depths = data['depths'].clone().detach().to(device=opt.device, dtype= torch.float)
         
-        with torch.cuda.amp.autocast(enabled=True):
-            embedding, pc_out = model(depths)
-            chamfer_loss = chamfer_distance(pc_out, test['gt_pc'].permute(0,2,1).to(device=opt.device, dtype= torch.float))[0]
-            with open(f'{opt.result_dir}/results.csv',"a") as f:
-                writer = csv.writer(f)
-                writer.writerow([f'{int(test["class_dir"])}', f'{(test["object_dir"][0])}', f'{float(chamfer_loss)}'])
+        if opt.fusion:
+            batch_colors = data['colors'].clone().detach().to(device=opt.device, dtype= torch.float32)
+            batch_masks = data['masks'].clone().detach().to(device=opt.device, dtype= torch.int64)
+            embedding, pc_out = model(colors = batch_colors, xyzs = depths, masks = batch_masks) # grid_out = unnormalized logits
+        else: embedding, pc_out = model(depths)
+        
+        chamfer_loss = chamfer_distance(pc_out, data['gt_pc'].permute(0,2,1).to(device=opt.device, dtype= torch.float))[0]
+        with open(f'{opt.result_dir}/results.csv',"a") as f:
+            writer = csv.writer(f)
+            writer.writerow([f'{int(data["class_dir"])}', f'{(data["object_dir"][0])}', f'{float(chamfer_loss)}'])
                 
-        #print("Predicted Reconstruction Chamfer Loss: ")
-        #print(chamfer_loss)
        
         if opt.visualize: 
             # Visualize result    
             pc_out = pc_out.squeeze(0).cpu().detach().numpy()
-            pc_gt = torch.Tensor.numpy(test['gt_pc']).transpose()
+            pc_gt = torch.Tensor.numpy(data['gt_pc']).transpose()
             pcd_gt = o3d.geometry.PointCloud()
             pcd_out = o3d.geometry.PointCloud()
             pcd_gt.points = o3d.utility.Vector3dVector(pc_gt)
             pcd_out.points = o3d.utility.Vector3dVector(pc_out)
             pcd_gt.colors = o3d.utility.Vector3dVector()
-            o3d.visualization.draw_geometries([pcd_out])
+            pcd_gt.paint_uniform_color([0, 0, 0])
+            pcd_out.paint_uniform_color([0, 1, 0])
+            o3d.visualization.draw_geometries([pcd_gt,pcd_out])
+            
+        if opt.save_renders:
+            #TODO: Add multi view support
+            pc_out = pc_out.squeeze(0).cpu().detach().numpy()
+            pc_gt = data['gt_pc'].squeeze(0).numpy().transpose()
+            if not os.path.exists(os.path.join(opt.result_dir, 'images')):
+                os.makedirs(os.path.join(opt.result_dir, 'images'))
+            vis.renderpoints(pc_out, path = os.path.join(opt.result_dir, 'images', f'pred_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
+            print('saved prediction: ', os.path.join(opt.result_dir, 'images', f'pred_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
+            vis.renderpoints(pc_gt, path = os.path.join(opt.result_dir, 'images', f'gt_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
+            print('saved gt: ', os.path.join(opt.result_dir, 'images', f'gt_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
+            
     return embedding
     
     
@@ -68,7 +89,7 @@ if __name__ == "__main__":
         model = PointCloudAE(emb_dim = opt.emb_dim, num_points= opt.num_points)
         
     try:
-        model= torch.nn.DataParallel(model)
+        #model= torch.nn.DataParallel(model)
         model.to(opt.device)
         model.load_state_dict(torch.load(opt.load_model))
         model.eval()
@@ -117,7 +138,7 @@ if __name__ == "__main__":
         embeddings = np.array(embeddings)
         if embeddings.shape[0]<=20:
             tsne = TSNE(2, perplexity=embeddings.shape[0]-1, verbose=0)
-        else: tsne = TSNE(2, perplexity=20, verbose=0)
+        else: tsne = TSNE(2, perplexity=30, verbose=0)
         tsne_proj = tsne.fit_transform(embeddings)
         cmap = cm.get_cmap('tab20')
         fig, ax = plt.subplots(figsize=(8,8))
@@ -128,6 +149,7 @@ if __name__ == "__main__":
             ax.scatter(tsne_proj[ix,0], tsne_proj[ix,1], label = g, s = 100)
         ax.legend(fontsize='large', markerscale=2)
         plt.show()
+        
     print(f'Inference finished: results saved to {opt.result_dir}/results.csv')
     print('------------------------------------------------------------------------------------------------------------------------------------')
     print('Results: ')
--- inference/inference_voxels.py
+++ inference/inference_voxels.py
@@ -12,31 +12,32 @@ import csv
 from ObjectReconstructor.datasets import BlenderDataset
 from ObjectReconstructor.models.voxel_ae import VoxelAE, VoxelFusionAE
 from ObjectReconstructor.configs import VOXEL_OCCUPANCY_TRESHOLD, VOXEL_RESOLUTION, VOXEL_SIZE
-from ObjectReconstructor.utils import pytorch3d_vis, voxel_to_pc, voxel_IoU
+from ObjectReconstructor.utils import pytorch3d_vis, voxel_to_pc, voxel_IoU, pc_down_sample
+from ObjectReconstructor.scratch import vis
 from pytorch3d.loss import chamfer_distance
 import pandas as pd
 from PIL import Image
+from torch.utils.data import random_split
 
-#cluster_views_15_emb_dim_1024_voxel_size_128_lr_0.001_epoch_1800.pth latest large train. 2300 also okay
-# most recent: cluster_views_15_emb_dim_1024_voxel_size_128_lr_0.001_epoch_2000.pth
 
-# single: single_object_cluster_views_15_emb_dim_1024_voxel_size_128_lr_0.001_epoch_2000.pth
-# single_object_Voxel_fusion_cluster_views_15_emb_dim_1024_voxel_size_128_lr_0.001_epoch_800.pth
 parser = argparse.ArgumentParser()
 parser.add_argument('--visualize', type=bool, default=False, help='Set to true if you want to visualize registration for each test object')
-parser.add_argument('--save_renders', type=bool, default=True, help='Render voxel grid reconstructions and save them to results folder')
+parser.add_argument('--save_renders', type=bool, default=False, help='Render voxel grid reconstructions and save them to results folder')
+parser.add_argument('--save_pc', type=bool, default=False, help='Render voxel grid reconstructions and save them to results folder')
 parser.add_argument('--tsne', type=bool, default=False, help='Set to true if you want to visualize T-SNE embeddings')
-parser.add_argument('--fusion', type=bool, default=True, help='Set to true if fusion model is to be used')
+parser.add_argument('--fusion', type=bool, default=False, help='Set to true if fusion model is to be used')
 parser.add_argument('--voxel_size', type=int, default=128, help='voxel grid resolution')
 parser.add_argument('--emb_dim', type=int, default=1024, help='dimension of latent embedding')
 parser.add_argument('--device', type=str, default='cuda', help='GPU to use')
-parser.add_argument('--load_model', type=str, default=f'{os.getcwd()}/results/voxels/models/single_object_Voxel_fusion_cluster_views_15_emb_dim_1024_voxel_size_128_lr_0.001_epoch_800.pth', help='load model path')
+parser.add_argument('--load_model', type=str, default=f'{os.getcwd()}/results/voxels/models/50%_cluster_views_15_emb_dim_1024_voxel_size_128_lr_0.001_epoch_1440.pth', help='load model path')
 parser.add_argument('--result_dir', type=str, default=f'{os.getcwd()}/inference/results/voxels', help='directory to save inference results')
-parser.add_argument('--save_dir', type=str, default=f'{os.getcwd()}/data/single_category_cups_train_test_split/test', help='save directory of images to be used for inference (preprocessed shapenet images)')
-parser.add_argument('--num_views', type=int, default=10, help = 'Number of input views per object instance, default set to 1 for single shot inference')
+parser.add_argument('--save_dir', type=str, default=f'{os.getcwd()}/data/large_train_test_split/test', help='save directory of images to be used for inference (preprocessed shapenet images)')
+parser.add_argument('--num_views', type=int, default=5, help = 'Number of input views per object instance, default set to 1 for single shot inference')
+
 
 names = {'03211117':'display', '02992529':'cellphone', '02954340':'cap', '02942699':'camera', '02946921':'can', '03797390':'mug', '02880940':'bowl',
-         '3211117':'display', '2992529':'cellphone', '2954340':'cap', '2942699':'camera', '2946921':'can', '3797390':'mug', '2880940':'bowl'}
+         '3211117':'display', '2992529':'cellphone', '2954340':'cap', '2942699':'camera', '2946921':'can', '3797390':'mug', '2880940':'bowl', '2876657':'bottle', '3642806':'laptop'}
+
 
 def inference(model, test_object):
     CE_loss = torch.nn.functional.binary_cross_entropy_with_logits
@@ -57,10 +58,9 @@ def inference(model, test_object):
         chamfer_loss = chamfer_distance(pc_out.unsqueeze(0), data['gt_pc'].permute(0,2,1).to(device=opt.device, dtype= torch.float))[0]
         IoU = voxel_IoU(grid_out.sigmoid().detach().cpu().numpy(), gt_grids.cpu().numpy())
         
-       #print('CE Loss: ', float(out_CE_loss), 'Chamfer Loss: ', float(chamfer_loss), 'IoU: ', IoU)
         with open(f'{opt.result_dir}/results.csv',"a") as f:
             writer = csv.writer(f)
-            writer.writerow([f'{int(data["class_dir"])}', f'{(data["object_dir"][0])}', f'{float(out_CE_loss)}', f'{float(chamfer_loss)}', f'{IoU}'])
+            writer.writerow([f'{names[str(int(data["class_dir"]))]}', f'{(data["object_dir"][0])}', f'{float(out_CE_loss)}', f'{float(chamfer_loss)}', f'{IoU}'])
                 
         if opt.visualize: 
             # Visualize result    
@@ -76,22 +76,34 @@ def inference(model, test_object):
             o3d.visualization.draw_geometries([pcd_gt,pcd_out])
             
         if opt.save_renders:
-            #TODO: Add multi view support
-            pose = data['poses'].squeeze(0).squeeze(0)
-            images = pytorch3d_vis(grid_out.sigmoid(), view_elev=20)
+            images = pytorch3d_vis(grid_out.sigmoid(), view_elev=20, azimuth=180)
             images = (images[0, ..., :3].cpu().numpy() *255).astype(np.uint8)
             im = Image.fromarray(images)
-            if not os.path.exists(os.path.join(opt.result_dir, 'images')):
-                os.makedirs(os.path.join(opt.result_dir, 'images'))
-            im.save(os.path.join(opt.result_dir, 'images', f'pred_{names[str(int(data["class_dir"]))]}_{str(data["object_dir"][0])}.png' ))
-            print('saved prediction: ', os.path.join(opt.result_dir, 'images', f'pred_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
-            gt = pytorch3d_vis(gt_grids, view_elev=20)
+            if not os.path.exists(os.path.join(opt.result_dir, 'images', 'out_voxels')):
+                os.makedirs(os.path.join(opt.result_dir, 'images', 'out_voxels'))
+            im.save(os.path.join(opt.result_dir, 'images', 'out_voxels', f'pred_angle_{i}_{opt.num_views}_{names[str(int(data["class_dir"]))]}_{str(data["object_dir"][0])}.png' ))
+            print('saved prediction: ', os.path.join(opt.result_dir, 'images','out_voxels', f'pred_angle_{i}_{opt.num_views}_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
+            
+            gt = pytorch3d_vis(gt_grids, view_elev=20, azimuth=180)
             gt = (gt[0, ..., :3].cpu().numpy() *255).astype(np.uint8)
             im = Image.fromarray(gt)
             if not os.path.exists(os.path.join(opt.result_dir, 'images')):
                 os.makedirs(os.path.join(opt.result_dir, 'images'))
-            im.save(os.path.join(opt.result_dir, 'images', f'gt_{names[str(int(data["class_dir"]))]}_{str(data["object_dir"][0])}.png' ))
-            print('saved gt: ', os.path.join(opt.result_dir, 'images', f'gt_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
+            im.save(os.path.join(opt.result_dir, 'images', 'out_voxels',f'gt_angle_{i}_{names[str(int(data["class_dir"]))]}_{str(data["object_dir"][0])}.png' ))
+            print('saved gt: ', os.path.join(opt.result_dir, 'images','out_voxels', f'gt_angle_{i}_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
+            
+        if opt.save_pc:
+            pc_out = pc_out.squeeze(0).cpu().detach().numpy()
+            pc_out ,_ = pc_down_sample(pc_out,2048)
+            pc_gt = data['gt_pc'].squeeze(0).numpy().transpose()
+            if not os.path.exists(os.path.join(opt.result_dir, 'images', 'point_clouds')):
+                os.makedirs(os.path.join(opt.result_dir, 'images', 'point_clouds'))
+            vis.showpoints(pc_out)
+            for i in range(0,360,20):
+                vis.renderpoints(pc_out, zangle = i, path = os.path.join(opt.result_dir, 'images', 'point_clouds', f'pred_angle_{i}_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
+            print('saved prediction: ', os.path.join(opt.result_dir, 'images', 'point_clouds', f'pred_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
+            vis.renderpoints(pc_gt, path = os.path.join(opt.result_dir, 'images', 'point_clouds', f'gt_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
+            print('saved gt: ', os.path.join(opt.result_dir, 'images', 'point_clouds' ,f'gt_{names[str(int(data["class_dir"]))]}_{data["object_dir"][0]}.png'))
             
     return embedding
     
@@ -124,11 +136,13 @@ if __name__ == "__main__":
         print('Loading model worked')
         print('------------------------------------------------------------------------------------------------------------------------------------')
     
-    test_dataset = BlenderDataset(mode = 'train', save_directory  = opt.save_dir, num_views=opt.num_views, voxel_size=opt.voxel_size)
+    test_dataset = BlenderDataset(mode = 'train', save_directory  = opt.save_dir, voxel_size=opt.voxel_size, num_views= opt.num_views)
     test_dataloader = torch.utils.data.DataLoader(test_dataset,
                                                    batch_size=1,
                                                    num_workers=1,
                                                    shuffle = False)
+    print('Current dataset len: ', test_dataset.__len__())
+    
     if opt.tsne:
         import matplotlib.pyplot as plt
         from sklearn.manifold import TSNE
@@ -136,7 +150,7 @@ if __name__ == "__main__":
         embeddings = torch.zeros((0, opt.emb_dim), dtype=torch.float32)
         labels = []
         categories = []
-        
+    
     for i, test_object in enumerate(test_dataloader):
         if i == 0: 
             if os.path.isfile(f'{opt.result_dir}/results.csv'):
@@ -150,26 +164,34 @@ if __name__ == "__main__":
             labels.append(int(np.array(test_object['class_dir'])))
             if test_object['class_dir'] not in categories:
                 categories.append(test_object['class_dir'])
-                
+    
+    np.save(f'{opt.result_dir}/tsne', np.array(embeddings), allow_pickle=True)
+    np.save(f'{opt.result_dir}/categories', np.array(labels), allow_pickle=True)
     # Plot T-SNE embedding space for dataset
     if opt.tsne:
+        import matplotlib.pyplot as plt
+        from sklearn.manifold import TSNE
+        from matplotlib import cm
+        embeddings = np.load(f'{opt.result_dir}/tsne.npy')
+        labels = np.load(f'{opt.result_dir}/categories.npy')
         embeddings = np.array(embeddings)
+        print('Num of embeddings: ', embeddings.shape[0])
         if embeddings.shape[0]<=20:
             tsne = TSNE(2, perplexity=embeddings.shape[0]-1, verbose=0)
-        else: tsne = TSNE(2, perplexity=20, verbose=0)
+        else: tsne = TSNE(2, perplexity=25, verbose=1) #75
         tsne_proj = tsne.fit_transform(embeddings)
         cmap = cm.get_cmap('tab20')
         fig, ax = plt.subplots(figsize=(8,8))
-        num_categories = len(categories)
         labels = np.array(labels)
         for g in np.unique(labels):
             ix = np.where(labels == g)
-            ax.scatter(tsne_proj[ix,0], tsne_proj[ix,1], label = g, s = 100)
+            ax.scatter(tsne_proj[ix,0], tsne_proj[ix,1], label = names[str(g)], s = 100)
         ax.legend(fontsize='large', markerscale=2)
-        plt.show()
+        plt.savefig(f'{opt.result_dir}/tsne.png')
     print(f'Inference finished: results saved to {opt.result_dir}/results.csv')
     print('------------------------------------------------------------------------------------------------------------------------------------')
     data = pd.read_csv(f'{opt.result_dir}/results.csv')
     print('Mean Cross-Entropy Loss: ', data['Cross Entropy Loss'].mean())
     print('Mean Chamfer Loss: ', data['Chamfer Loss'].mean())
     print('Mean IoU: ', data['IoU'].mean()) 
+    print(data.groupby(by='Class').mean())
